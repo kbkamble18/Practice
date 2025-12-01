b63a5f4c6884ebd11761392ef92e5a17f06d9c50
Karan Kamble <kbkamble18@gmail.com>
2025-12-01 13:35:32 +0530

    Add files via upload

---
+++ Prac_14.py
@@ -0,0 +1,24 @@
+# List
+
+# num = [10,20,30,40,50]
+# print(num[0::2])
+# num.append(80)
+# print(num)
+# num.insert(5,60)
+# print(num)
+
+# a =[1,22,34,44,22,34]
+# n=list(set(a))
+# print(n)
+# print(max(a))
+
+# Tuple
+
+# t =(11,22,33)
+# print(t[-1])
+# print(len(t))
+# print(t.count(4))
+# print(33 in t)
+
+# Set
+
