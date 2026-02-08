f31bdc20847bb104e6d82959ee7f44771d1b1ded
Karan Kamble <kbkamble18@gmail.com>
2026-02-09 01:01:27 +0530

    Add files via upload

--- Prac_13.py
+++ Prac_13.py
@@ -1,1 +1,14 @@
-print("Hello World")
+'''print("Hello World")
+hello = 'kim'
+world = 'hello'
+world = hello
+hello = 'no'
+print(hello, world)
+name = input('Name :')
+print("Hello",name)
+'''
+
+y = int(input("1 st no."))
+x = int(input("2 nd no."))
+result = x+y
+print(result)
