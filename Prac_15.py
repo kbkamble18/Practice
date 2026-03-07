df8fb79676185f9d9e911794145ab1a082a575ea
Karan Kamble <kbkamble18@gmail.com>
2026-03-07 23:20:06 +0530

    Add files via upload

--- Prac_15.py
+++ Prac_15.py
@@ -7,6 +7,22 @@
 # name = input("Enter your name")
 # print(name)
 
-r= 5
-for i in range(1, r+1):
-    print("*"*i)
+# r= 5
+# for i in range(1, r+1):
+#    print("*"*i)
+
+# Recommended: install if not already done (run in terminal once)
+# pip install sqlalchemy pandas pymysql
+
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Replace these with your actual details
+engine = create_engine(
+    "mysql+pymysql://your_username:your_password@localhost/your_database_name"
+)
+
+# Example query - change table name as needed
+df = pd.read_sql("SELECT * FROM your_table LIMIT 10", engine)
+
+print(df)
